f120472e28bc42901181235004d026ee26e20ce2
MahmoudAbdelfattah <mahmoudabdelftah20120@gmail.com>
2019-11-19 23:03:01 +0300

    this is the first commit

---
+++ Run.bat
@@ -0,0 +1,2 @@
+cd E:\automation\java automation projects\HybridFrameworkAutomationTestingExample
+mvn clean install
---
+++ src/main/java/HybridFrameworkAutomationTestingExample/HybridFrameworkAutomationTestingExample/App.java
@@ -0,0 +1,13 @@
+package HybridFrameworkAutomationTestingExample.HybridFrameworkAutomationTestingExample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
